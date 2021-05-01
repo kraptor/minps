e9e8ea02c67888cfb80e66aa4ab53c9b2130bcb1
kraptor <kraptor@kraptor.com>
2021-05-01 09:54:02 +0200

    concepts: add tests for component concepts

---
+++ tests/test_concepts.nim
@@ -0,0 +1,29 @@
+# Copyright (c) 2021 kraptor
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+import unittest
+
+import emulator/[
+    concepts, cpu, mmu, bios, platform
+]
+
+suite "Test concepts":
+
+    test "CPU":
+        assert Cpu is Component
+
+    test "MMU":
+        assert Mmu is Resettable
+        assert Mmu is Readable
+        # assert Mmu is Writable
+
+    test "BIOS":
+        assert Bios is Component
+        # assert Bios is Readable
+        # assert Bios isnot Writable
+
+    test "Platform":
+        assert Platform is Runnable
+        assert Platform is Resettable
