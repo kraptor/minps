4899e1e190974dee220a3b9249fba22e1ce8f9f7
kraptor <kraptor@kraptor.com>
2021-05-19 14:15:10 +0200

    platform: add support to run a program (not an exe, yet)

--- src/emulator/platform.nim
+++ src/emulator/platform.nim
@@ -5,8 +5,9 @@
 
 {.experimental: "codeReordering".}
 
-import ../core/[log]
+import ../core/[log, util]
 import cpu/cpu
+import cpu/assembler
 import mmu
 import bios
 
@@ -15,8 +16,8 @@ logChannels ["platform"]
 
 type
     Platform* = ref object
-        cpu: Cpu
-        mmu: Mmu
+        cpu*: Cpu
+        mmu*: Mmu
 
 
 proc New*(T: type Platform, bios: Bios = nil): Platform =
@@ -41,4 +42,14 @@ proc Reset*(self: Platform) =
 proc Run*(self: Platform) =
     while true:
         self.cpu.RunNext()
-    
+
+
+proc RunFor*(self: Platform, number_of_instructions: int64) =
+    while self.cpu.stats.instruction_count < number_of_instructions:
+        self.cpu.RunNext()
+
+
+proc RunProgram*(self: Platform, program: Program) =
+    self.Reset()
+    self.mmu.bios = Bios.FromProgram(program)
+    self.RunFor(program.len)    
