b86f825b9bf2e0024e077efd6322c18b18a6d6eb
kraptor <kraptor@kraptor.com>
2021-05-19 18:56:18 +0200

    assember: refactor into I/R procs and add OR support

--- src/emulator/cpu/assembler.nim
+++ src/emulator/cpu/assembler.nim
@@ -13,31 +13,37 @@ type
     Program* = seq[Instruction]
 
 
-proc lui*(target: CpuRegisterIndex, immediate: uint16): Instruction =
-    result.I.opcode = LUI
-    result.I.rt = target.uint8
-    result.I.imm16 = immediate
+proc I(opcode: Opcode, rt, rs: CpuRegisterIndex, imm16: uint16): Instruction =
+    result.I.opcode = opcode
+    result.I.rt = rt.uint8
+    result.I.rs = rs.uint8
+    result.I.imm16 = imm16
 
 
-proc ori*(target, source: CpuRegisterIndex, value: uint16): Instruction =
-    result.I.opcode = ORI
-    result.I.rt = target.uint8
-    result.I.rs = source.uint8
-    result.I.imm16 = value
-
-
-proc sll*(target, source: CpuRegisterIndex, amount: 0..0b11111): Instruction =
-    result.R.opcode = Special
-    result.R.function = SLL
-    result.R.rd = target.uint8
-    result.R.rt = source.uint8
+proc R(function: Function, rs, rt, rd: CpuRegisterIndex, amount: 0..0b11111): Instruction =
+    result.R.opcode = Opcode.Special
+    result.R.function = function
+    result.R.rs = rs.uint8
+    result.R.rt = rt.uint8
+    result.R.rd = rd.uint8
     result.R.shamt = amount.uint8
 
 
-proc addiu*(target, source, immediate: uint16): Instruction =
-    result.I.opcode = ADDIU
-    result.I.rt = target.uint8
-    result.I.rs = source.uint8
-    result.I.imm16 = immediate
+proc LUI*(target: CpuRegisterIndex, imm: uint16): Instruction =
+    I(Opcode.LUI, target, 0, imm)
 
 
+proc ORI*(target, source: CpuRegisterIndex, imm: uint16): Instruction =
+    I(Opcode.ORI, target, source, imm)
+
+
+proc SLL*(target, source: CpuRegisterIndex, amount: 0..0b11111): Instruction =
+    R(Function.SLL, 0, source, target, amount)
+
+
+proc ADDIU*(target, source, imm: uint16): Instruction =
+    I(Opcode.ADDIU, target, source, imm)
+
+
+proc OR*(target, source_a, source_b: CpuRegisterIndex): Instruction =
+    R(Function.OR, source_a, source_b, target, 0)
