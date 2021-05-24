7fb221287b20ca2000e4d1e2e2c080f38e4c2c72
kraptor <kraptor@kraptor.com>
2021-05-24 22:08:10 +0200

    cop0: implement DCIC register stub

--- src/emulator/cpu/cop0.nim
+++ src/emulator/cpu/cop0.nim
@@ -25,7 +25,7 @@ type
         r4: uint32
         BDA: uint32
         JUMPDEST: uint32
-        DCIC: uint32
+        DCIC: Cop0DCICRegister
         BadVaddr: uint32
         BDAM: uint32
         r10: uint32
@@ -70,7 +70,7 @@ type
         KernelOnly = 0
         KernelAndUser
 
-    Cop0SystemStatusRegister* {.packed.} = object
+    Cop0SystemStatusRegister* {.packed.} = object   # APU Status Register
         IEc     {.bitsize: 1.}: InterruptEnableMode # Current Interrupt Enable
         KUc     {.bitsize: 1.}: KernelMode          # Current Kernel/User Mode
         IEp     {.bitsize: 1.}: InterruptEnableMode # Previous Interrupt Enable
@@ -99,6 +99,35 @@ type
         CU3     {.bitsize: 1.}: CoprocessorEnableMode
 
 
+type 
+    JumpRedirectionMode* {.pure.} = enum
+        Disabled
+        Enabled01
+        Enabled02
+        Enabled03
+
+    Cop0DCICRegister* {.packed.} = object # Debug and Cache Invalidate Control Register
+        DB      {.bitsize: 1.}: bool # Any break - Debug flag, set to true when detected any debug condition
+        PC      {.bitsize: 1.}: bool # BPC Code Break - Set on PC debug condition
+        DA      {.bitsize: 1.}: bool # BDA Data Break - Set on Data address debug condition
+        R       {.bitsize: 1.}: bool # BDA Data READ Break
+        W       {.bitsize: 1.}: bool # BDA Data WRITE Break
+        T       {.bitsize: 1.}: bool # Jump Break
+        ZERO_06_11      {.bitsize: 6.}: uint8 # Always read as zero. Writes are ignored.
+        JumpRedirection {.bitsize: 2.}: JumpRedirectionMode
+        UNKNOWN_14_15   {.bitsize: 2.}: uint8 
+        ZERO_16_22      {.bitsize: 7.}: uint8
+        DE      {.bitsize: 1.}: bool # Debug Enable - Super-Master Enable 1 for bit 24 - 29
+        PCE     {.bitsize: 1.}: bool # Execution Breakpoint - Program Counter Breakpoint Enable
+        DAE     {.bitsize: 1.}: bool # Data Access Breakpoint Enable
+        DR      {.bitsize: 1.}: bool # Data Read Enable - Break on data read
+        DW      {.bitsize: 1.}: bool # Data Write Enable
+        TE      {.bitsize: 1.}: bool # Trace Enable - Break on any jump
+        KD      {.bitsize: 1.}: bool # Kernel Debug Enable
+        UD      {.bitsize: 1.}: bool # User Debug Enable
+        TR      {.bitsize: 1.}: bool # Trap Enable
+
+
 proc IsolateCacheEnabled*(cop0: Cop0): bool = cop0.parts.SR.Isc
 
 
@@ -120,6 +149,7 @@ proc ReadRegisterDebug*(self: var Cop0, r: Cop0RegisterIndex): uint32 =
         # TODO: return garbage, but don't trigger exception
         NOT_IMPLEMENTED
     else:
+        warn fmt"cop0[{r.Cop0RegisterName}] read."
         return self.regs[r]
 
 
@@ -128,7 +158,12 @@ proc WriteRegisterDebug*(self: var Cop0, r: Cop0RegisterIndex, v: uint32) =
 
     case r.Cop0RegisterName:
     of SR:
-        let sr = self.parts.SR
+        var sr = self.parts.SR
+
+        # TS bit is always 0 as per documentation
+        assert sr.TS == false, "Cop0.SR.TS is read-only. Use cop0.SetTLBShutdown(true) from emu code instead?"
+        sr.TS = false
+
         notice fmt"Cop0 Status Register set to: {v:08x}h"
         notice fmt"- IEc (Current)    : {sr.IEc}"
         notice fmt"- KUc (Current)    : {sr.KUc}"
@@ -148,11 +183,40 @@ proc WriteRegisterDebug*(self: var Cop0, r: Cop0RegisterIndex, v: uint32) =
         notice fmt"- CU0 (COP0 Mode)  : {sr.CU0}"
         notice fmt"- CU1 (COP1 Mode)  : {sr.CU1}"
         notice fmt"- CU2 (COP2 Mode)  : {sr.CU2}"
-        notice fmt"- CU3 (COP3 Mode)  : {sr.CU3}"
+        notice fmt"- CU3 (COP3 Mode)  : {sr.CU3}"       
 
-        warn fmt"cop0reg[SR] set to: {v:08x}h. Side-effects are not implemented."
+        warn fmt"cop0[SR] set to: {v:08x}h. Side-effects are not implemented."
+    of DCIC:
+        var dcic = self.parts.DCIC
+
+        # TODO: use here a mask if this is ever a bottleneck
+        dcic.ZERO_06_11 = 0
+        dcic.ZERO_16_22 = 0
+
+        notice fmt"Cop0 Debug Register (DCIC) set to: {v:08x}h"
+        notice fmt"- DB (Debug)              : {dcic.DB}"
+        notice fmt"- PC (Program Counter)    : {dcic.PC}"
+        notice fmt"- DA (Data Address)       : {dcic.DA}"
+        notice fmt"- R  (Read Reference)     : {dcic.R}"
+        notice fmt"- W  (Write Reference)    : {dcic.W}"
+        notice fmt"- T  (Trace)              : {dcic.T}"
+        # notice fmt"- ZERO_06_11      : {dcic.ZERO_06_11}"
+        notice fmt"- Jump Redirection        : {dcic.JumpRedirection}"
+        notice fmt"- UNKNOWN_14_15           : {dcic.UNKNOWN_14_15}"
+        # notice fmt"- ZERO_16_22      : {dcic.ZERO_16_22}"
+        notice fmt"- DE (Debug Enable)       : {dcic.DE}"
+        notice fmt"- PCE (PC Break Enable)   : {dcic.PCE}"
+        notice fmt"- DAE (DA Break Enable)   : {dcic.DAE}"
+        notice fmt"- DR (Data Read Enable)   : {dcic.DR}"
+        notice fmt"- DW (Data Write Enable)  : {dcic.DW}"
+        notice fmt"- TE (Trace Enable)       : {dcic.TE}"
+        notice fmt"- KD (Kernel Debug Enable): {dcic.KD}"
+        notice fmt"- UD (User Debug Enable)  : {dcic.UD}"
+        notice fmt"- TR (Trap Enable)        : {dcic.TR}"
+
+        warn fmt"cop0[DCIC] set to: {v:08x}h. Side-effects are not implemented."
     else:
-        NOT_IMPLEMENTED
+        NOT_IMPLEMENTED fmt"Cop0 Register write not implemented: {r.Cop0RegisterName}"
 
 
 proc WriteRegister*(self: var Cop0, r: Cop0RegisterIndex, v: uint32) =
