515403eb80f1f8769e77ca441d9995c573f41ec7
kraptor <kraptor@kraptor.com>
2021-05-26 13:46:41 +0200

    cpu: add s8 alias to fp register name

--- src/emulator/cpu/cpu.nim
+++ src/emulator/cpu/cpu.nim
@@ -47,7 +47,7 @@ proc GetCpuRegisterAlias*(r: CpuRegisterIndex): string =
         "zero", "at", "v0", "v1", "a0", "a1", "a2", "a3", 
         "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", 
         "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", 
-        "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"]
+        "t8", "t9", "k0", "k1", "gp", "sp", "fp_s8", "ra"]
     CPU_REGISTER_TO_ALIAS[r]
 
 
