33b829ed3004dd49cc1d97071e1614c9df7616da
kraptor <kraptor@kraptor.com>
2021-05-26 21:52:04 +0200

    project: update callgrind support

--- tools/run_callgrind.sh
+++ tools/run_callgrind.sh
@@ -4,8 +4,9 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-rm -f callgrind.out.*
+pwd
+rm -f callgrind.out*
 nimble build_callgrind
-bin/minps_callgrind
 valgrind --tool=callgrind bin/minps_callgrind
+python ../nim_callgrind/nim_callgrind.py `ls callgrind.out.*` callgrind.out
 kcachegrind
