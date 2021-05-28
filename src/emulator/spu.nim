dbf866a705cd2e4956ba0b849a835df85efc6980
kraptor <kraptor@kraptor.com>
2021-05-28 11:36:47 +0200

    spu: remove extra copyright comments

--- src/emulator/spu.nim
+++ src/emulator/spu.nim
@@ -3,16 +3,6 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-# Copyright (c) 2021 kraptor
-# 
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-# Copyright (c) 2021 kraptor
-# 
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
 {.experimental: "codeReordering".}
 
 import strformat
