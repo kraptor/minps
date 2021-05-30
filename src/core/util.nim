92ed800866900194530e41655de2d1e7f548960a
kraptor <kraptor@kraptor.com>
2021-05-30 10:48:44 +0200

    util: improve string allocations when using NOT_IMPLEMENTED

--- src/core/util.nim
+++ src/core/util.nim
@@ -11,6 +11,13 @@ const
 
 
 template NOT_IMPLEMENTED*(message: string = "") =
-    error NOT_IMPLEMENTED_PREFIX & message
-    raise NotImplementedDefect(msg: message)
+    # by using a block we force the Nim compiler to allocate
+    # the string only within the block, therefore only when
+    # the code hits the NOT_IMPLEMENTED is allocated. Because
+    # this template is used very often during development
+    # by issuing a block we save lots and lots of string
+    # allocs/deallocs
+    block:
+        error NOT_IMPLEMENTED_PREFIX & message
+        raise NotImplementedDefect(msg: message)
 
