cba21f14d00957fee6de4f97cf887792ae27e105
kraptor <kraptor@kraptor.com>
2021-06-09 22:55:10 +0200

    bios: add syscall function names

---
+++ src/emulator/bios/syscalls.nim
@@ -0,0 +1,36 @@
+# Copyright (c) 2021 kraptor
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+import ../../core/log
+import ../../core/util
+import ../cpu/cpu
+import ../address
+
+
+logChannels ["bios", "syscall"]
+
+
+type
+    SyscallFunction = enum
+        NoFunction = 0
+        EnterCriticalSection = 1
+        ExitCriticalSection = 2
+        ChangeThreadSubFunction = 3
+
+
+proc SyscallFunctionName*(cpu: Cpu): string =
+    case cpu.ReadRegisterDebug(4):
+    of ord NoFunction: return "NoFunction()"
+    of ord EnterCriticalSection: return "EnterCriticalSection()"
+    of ord ExitCriticalSection: return "ExitCriticalSection()"
+    of ord ChangeThreadSubFunction: return "ChangeThreadSubFunction(" & $cpu.ReadRegisterDebug(5).Address & ")"
+    else:
+        return "DeliverEvent(F0000010h, 4000h)"
+
+
+proc logSyscallName*(cpu: Cpu) =
+    trace SyscallFunctionName(cpu)
+    echo "BIOS/SYS " & SyscallFunctionName(cpu)
+    # NOT_IMPLEMENTED
