264d45dfb28e18f90b0ee2b5418534ec85959b44
kraptor <kraptor@kraptor.com>
2021-06-10 21:09:12 +0200

    project: rework main to allow GUI and CLI

--- src/minps.nim
+++ src/minps.nim
@@ -8,29 +8,44 @@
 include inc/profiler # NOTE: should be an include for it to work
 include inc/concept_check # NOTE: we want to check concepts asap
 
-import core/[log, version]
-import emulator/[config, platform]
-import emulator/bios/bios
+import core/log
+import core/config
+import core/version
+
+
+from ui/cli import nil
+from ui/gui import nil
+
 
 logFile "minps.log"
 logChannels ["main"]
 
 
-proc main() =
+type
+    RunMode = enum
+        Console
+        Gui
+
+
+proc main(run_mode: RunMode) =
     logEcho "minps - a wannabe PlayStation 1 emulator"
     logEcho "version: " & VersionString
 
-    notice "minps started"
-    let config = Config.New("minps.ini")
-    var platform = Platform.New(
-        Bios.FromFile(config.bios_file)
-    )
-    platform.Run()
-    notice "minps stopped"
+    var config = Config.New("minps.ini")
+    
+    
+    case run_mode:
+    of Console:
+        notice "Using command-line interface."
+        cli.main(config)
+    of Gui:
+        notice "Using GUI interface"
+        gui.main(config)
 
 
 when isMainModule:
     try:
-        main()
+        # TODO: add switch to use cli/gui mode
+        main(Gui)
     finally:
         logFinalize()
