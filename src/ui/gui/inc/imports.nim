95810980485e6380bfca48d9419cd8eeed41ac76
kraptor <kraptor@kraptor.com>
2021-06-11 18:43:04 +0200

    gui: add common imports together

---
+++ src/ui/gui/inc/imports.nim
@@ -0,0 +1,12 @@
+# Copyright (c) 2021 kraptor
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+{. warning[UnusedImport]:off .}
+
+import nimgl/[glfw, opengl] 
+import nimgl/imgui, nimgl/imgui/[impl_opengl, impl_glfw]
+import ../../../core/log
+import ../../../core/config
+import ../../../core/version
