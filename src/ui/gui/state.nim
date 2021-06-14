87c7179b1333206b69788998ac240e3ed7a77176
kraptor <kraptor@kraptor.com>
2021-06-14 20:44:37 +0200

    gui: add platform to state

--- src/ui/gui/state.nim
+++ src/ui/gui/state.nim
@@ -4,9 +4,11 @@
 # https://opensource.org/licenses/MIT
 
 include inc/imports
+import ../../emulator/platform
 
 type
     State* = object
-        window  *: GLFWWindow
-        context *: ptr ImGuiContext
-        config  *: Config
+        window   *: GLFWWindow
+        context  *: ptr ImGuiContext
+        config   *: Config
+        platform *: Platform
