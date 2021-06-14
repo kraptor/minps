09bd91949e17e170923576ad3e78ad516f6b6e44
kraptor <kraptor@kraptor.com>
2021-06-14 17:38:17 +0200

    gui: use new menuitem widget

--- src/ui/gui/mainmenu.nim
+++ src/ui/gui/mainmenu.nim
@@ -5,23 +5,15 @@
 
 include inc/imports
 import nimgl/imgui
+
 import state
-import actions
 import widgets
-
-
-template menuitem(state: var State, action_name: string) =
-    let action = gui_actions[action_name]
-    if igMenuItem(action.label, action.shortcut, action.IsSelected(state), action.IsEnabled(state)):
-        action.Run(state)
 
 
 proc draw*(state: var State) =
     var state = state
     if igBeginMainMenuBar():
         if igBeginMenu("MinPS"):
-            # if igMenuItem("Quit"):
-            #     gui_actions["quit"].Run(state)
             menuitem(state, "quit")
             igEndMenu()
         igEndMainMenuBar()
