ba614e0b936c037b605c34780e18e40a6ea70b05
kraptor <kraptor@kraptor.com>
2021-06-14 17:38:03 +0200

    gui: start building library of useful widget templates

--- src/ui/gui/widgets.nim
+++ src/ui/gui/widgets.nim
@@ -3,3 +3,15 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+include inc/imports
+
+import state
+import actions
+export state
+export actions
+
+
+template menuitem*(state: var State, action_name: string) =
+    let action = gui_actions[action_name]
+    if igMenuItem(action.label, action.shortcut, action.IsSelected(state), action.IsEnabled(state)):
+        action.Run(state)
