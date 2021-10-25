00abc7ae85d55a53cec834afe541d28f52abf5e1
kraptor <kraptor@kraptor.com>
2021-10-25 23:38:34 +0200

    gui: add code for future support of extra font symbols

--- src/ui/gui/fonts.nim
+++ src/ui/gui/fonts.nim
@@ -9,9 +9,33 @@ import tables
 var
     fonts: Table[string, ptr ImFont]
 
-proc LoadFont*(name: string, font: FontConfig) =
-    fonts[name] = igGetIO().fonts.addFontFromFileTTF(font.file.cstring, font.size)
+const 
+    USE_EXTRA_SYMBOLS = false
 
+proc LoadFont*(name: string, font: FontConfig) =
+    when USE_EXTRA_SYMBOLS:
+        # TODO: to use extra symbols, NimGL ImGUI bindings need to be updated 
+        #       with missing functions and types:
+        type
+            ImVectorImWchar {.importc: "ImVector_ImWchar"} = object
+                size* {.importc: "Size".}: int32
+                capacity* {.importc: "Capacity".}: int32
+                data* {.importc: "Data".}: ptr ImWchar 
+
+        proc newImVectorImWchar(): ptr ImVectorImWchar {.importc: "ImVector_ImWchar_create".}
+        proc newImFontGlyphRangesBuilder(): ptr ImFontGlyphRangesBuilder {.importc: "ImFontGlyphRangesBuilder_ImFontGlyphRangesBuilder".}
+        proc buildRanges(self: ptr ImFontGlyphRangesBuilder, out_ranges: ptr ImVectorImWchar): void {.importc: "ImFontGlyphRangesBuilder_BuildRanges".}
+
+        var 
+            ranges = newImVectorImWchar()
+            builder = newImFontGlyphRangesBuilder()
+        builder.addText("│┃‖")
+        builder.addRanges(igGetIO().fonts.getGlyphRangesDefault)        
+        builder.buildRanges(ranges)
+        fonts[name] = igGetIO().fonts.addFontFromFileTTF(font.file.cstring, font.size, nil, ranges.data)
+    else:
+        fonts[name] = igGetIO().fonts.addFontFromFileTTF(font.file.cstring, font.size)
+    
 
 proc GetFont*(name: string): ptr ImFont =
     fonts[name]
