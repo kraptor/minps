e2c972abbd8ec19b3bf3777c7a83b23330089644
kraptor <kraptor@kraptor.com>
2021-10-31 11:52:56 +0100

    gui: dim zero values in cpu registers window

--- src/ui/gui/cpu_registers.nim
+++ src/ui/gui/cpu_registers.nim
@@ -12,18 +12,46 @@ import ../../emulator/cpu/instruction
 import ../../emulator/cpu/disassembler
 import ../../emulator/mmu
 import ../../core/util
+
 import state
+import colors
 import widgets
 
 
+template text_color*(text: string, color: ImVec4) =
+    igTextColored color, text.cstring
+
+
+proc text_color*(enabled: bool, text: string, true_color: ImVec4, false_color: ImVec4) =
+    if enabled:
+        text_color text, true_color
+    else:
+        text_color text, false_color
+
+
+proc text_color*(enabled: bool, text: string, true_color: ImVec4) =
+    if enabled:
+        text_color text, true_color
+    else:
+        text text
+
+
 proc Draw*(state: var State) =
-    begin "CPU: Registers", state.config.gui.registers.window_visible:
+    begin "CPU: Registers", state.config.gui.registers.window_visible, AlwaysAutoResize:
         font "mono":
             var cpu = state.platform.cpu
 
             text fmt"{""id"":>2} {""alias"":>5} {""hex"":>8} {""int32"":>11} {""uint32"":>11}"
             `----`
-            for index, register in cpu.regs:
-                text fmt"{index:>2} {GetCpuRegisterAlias(index):>5} {register:>8x} {cast[int32](register):>11} {register:>11}"
+            for register, value in cpu.regs:
+                text fmt"{register:>2} "
+                sameline
+                text fmt"{GetCpuRegisterAlias(register):>5} "
+                sameline
+                text_color value == 0, fmt"{value:>8x}", colors.ZERO_VALUE
+                sameline
+                text_color value == 0, fmt"{cast[int32](value):>11} ", colors.ZERO_VALUE
+                sameline
+                text_color value == 0, fmt"{value:>11}", colors.ZERO_VALUE
             `----`
 
