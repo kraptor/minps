efd730d31bedfcefd27af47ce98a3b0fa5d45a3c
kraptor <kraptor@kraptor.com>
2021-11-01 20:59:11 +0100

    core: add some more standard colors

--- src/core/colors.nim
+++ src/core/colors.nim
@@ -22,9 +22,23 @@ proc color*(r: uint8, g: uint8, b: uint8, a: uint8 = 255): Color =
     (r:r.float32/255'f32, g:g.float32/255'f32, b:b.float32/255'f32, a:a.float32/255'f32)
 
 
+# predefined colors
 const
-    # Predefined colors
-    GRAY_DIMMED* = color(0.2, 0.2, 0.2)
+    # basic colors
+    WHITE      * = color(1.0, 1.0, 1.0)
+    BLACK      * = color(0.0, 0.0, 0.0)
+    GRAY       * = color(0.5, 0.5, 0.5)
+    GRAY_DIMMED* = color(0.35, 0.35, 0.35)
+    RED        * = color(1.0, 0.0, 0.0)
+    GREEN      * = color(0.0, 1.0, 0.0)
+    BLUE       * = color(0.0, 0.0, 1.0)
+
+    # palette colors
+    PALETTE_DARK    * = color(  1,  22,  39)
+    PALETTE_LIGHT   * = color(253, 255, 252)
+    PALETTE_TEALISH * = color( 46, 196, 182)
+    PALETTE_REDISH  * = color(231,  29,  54)
+    PALETTE_ORANGISH* = color(255, 159,  28)
 
 
 
