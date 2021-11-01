0523498813ed581fba0fc61c0c24cc0095af46e1
kraptor <kraptor@kraptor.com>
2021-11-01 21:04:10 +0100

    gui: overhaul COP0 register display

--- src/ui/gui/cop0_registers.nim
+++ src/ui/gui/cop0_registers.nim
@@ -21,21 +21,22 @@ import widgets
 proc Draw*(state: var State) =
     begin "COP0: Registers", state.config.gui.cop0_registers.window_visible, AlwaysAutoResize:
         font "mono":
-            var cop0 = state.platform.cpu.cop0
-            var cfg = state.config
+            let
+                cop0 = state.platform.cpu.cop0
+                palette = state.config.gui.palette
 
-            text fmt"{""id"":>2} {""alias"":>9} {""hex"":>8} {""int32"":>11} {""uint32"":>11}"
-            `----`
-            for register, value in cop0.regs:
-                var name = $register
-                text fmt"{register:>2} "
-                sameline
-                text fmt"{GetCop0RegisterAlias(register):>9} "
-                sameline
-                text_color value == 0, fmt"{value:>8x} ", cfg.gui.palette.ZERO_VALUE
-                sameline
-                text_color value == 0, fmt"{cast[int32](value):>11} ", cfg.gui.palette.ZERO_VALUE
-                sameline
-                text_color value == 0, fmt"{value:>11}", cfg.gui.palette.ZERO_VALUE
-            `----`
+            table "cop0_registers", 5:
+                table_setup_column fmt"{""id"":>3}"
+                table_setup_column fmt"{""name"":>6}"
+                table_setup_column fmt"{""hex"":>8}"
+                table_setup_column fmt"{""int32"":>10}"
+                table_setup_column fmt"{""uint32"":>10} "
+                table_header_row_draw()
 
+                for register, value in cop0.regs:
+                    table_next_row()
+                    table_next_column(); text_color fmt"{register:>3}", palette.REGISTER_NUMBER
+                    table_next_column(); text_color fmt"{GetCop0RegisterAlias(register):>6}", palette.REGISTER_NAME
+                    table_next_column(); text_color value == 0, fmt"{value:>8x}", palette.ZERO_VALUE
+                    table_next_column(); text_color value == 0, fmt"{cast[int32](value):>10}", palette.ZERO_VALUE
+                    table_next_column(); text_color value == 0, fmt"{value:>10} ", palette.ZERO_VALUE
