f6d3795b99f273e973da28da6c05a699f743ef4a
kraptor <kraptor@kraptor.com>
2021-11-01 21:03:37 +0100

    gui: overhaul CPU registers display

--- src/ui/gui/cpu_registers.nim
+++ src/ui/gui/cpu_registers.nim
@@ -17,23 +17,64 @@ import state
 import widgets
 
 
+proc draw_register_row(id: string, name: string, value: uint32, palette: ColorPalette) =
+    table_next_row()
+    table_next_column(); text_color fmt"{id:>3}", palette.REGISTER_NUMBER
+    table_next_column(); text_color fmt"{name:>6}", palette.REGISTER_NAME
+    table_next_column(); text_color value == 0, fmt"{value:>8x}", palette.ZERO_VALUE
+    table_next_column(); text_color value == 0, fmt"{cast[int32](value):>10}", palette.ZERO_VALUE
+    table_next_column(); text_color value == 0, fmt"{value:>10} ", palette.ZERO_VALUE
+
+
+proc draw_register_row(name: string, value: uint32, palette: ColorPalette) =
+    draw_register_row("", name, value, palette)
+
+
 proc Draw*(state: var State) =
     begin "CPU: Registers", state.config.gui.registers.window_visible, AlwaysAutoResize:
         font "mono":
             var cpu = state.platform.cpu
             var cfg = state.config
 
-            text fmt"{""id"":>2} {""alias"":>5} {""hex"":>8} {""int32"":>11} {""uint32"":>11}"
-            `----`
-            for register, value in cpu.regs:
-                text fmt"{register:>2} "
-                sameline
-                text fmt"{GetCpuRegisterAlias(register):>5} "
-                sameline
-                text_color value == 0, fmt"{value:>8x}", cfg.gui.palette.ZERO_VALUE
-                sameline
-                text_color value == 0, fmt"{cast[int32](value):>11} ", cfg.gui.palette.ZERO_VALUE
-                sameline
-                text_color value == 0, fmt"{value:>11}", cfg.gui.palette.ZERO_VALUE
-            `----`
+            table "registers", 5:
+                table_setup_column fmt"{""id"":>3}"
+                table_setup_column fmt"{""name"":>6}"
+                table_setup_column fmt"{""hex"":>8}"
+                table_setup_column fmt"{""int32"":>10}"
+                table_setup_column fmt"{""uint32"":>10} "
+                table_header_row_draw()
+
+                for register, value in cpu.regs:
+                    draw_register_row(
+                        $register, 
+                        GetCpuRegisterAlias(register), 
+                        value, 
+                        cfg.gui.palette
+                    )
+                `----`
+
+                # HI register
+                block hi_register:
+                    let 
+                        hi_data = state.platform.cpu.ReadHiRegister()
+                        value = if hi_data.cycles >= 0: 0'u32 else: hi_data.value
+                    draw_register_row("HI", value, cfg.gui.palette)
+                
+                # LO register
+                block lo_register:
+                    let
+                        lo_data = state.platform.cpu.ReadHiRegister()
+                        value = if lo_data.cycles >= 0: 0'u32 else: lo_data.value
+                    draw_register_row("LO", value, cfg.gui.palette)
+
+                `----`
+
+                # PC register
+                table_next_row()
+                table_next_column(); text "   "
+                table_next_column(); text_color fmt"{""PC"":>6}", cfg.gui.palette.REGISTER_NAME
+                table_next_column(); text fmt"{state.platform.cpu.pc.uint32:>8x}"
+                table_next_column();
+                table_next_column();
+
 
